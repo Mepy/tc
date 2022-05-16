3cdd0efec6ef25e19cfad4dc282553832cd541be
Mepy <angel@mepy.net>
2022-05-16 09:05:02 +0800

    ast 0.3.E impl ir::Sort simply; builtin geti, puti, puts; fix bugs for block ID

--- src/ast/api.cpp
+++ src/ast/api.cpp
@@ -16,20 +16,55 @@ void test_imm(API& context);
 void test_array(API& context);
 void test_delay_func_type(API& context);
 void test_match(API& context);
-void test_i2f_f2i(API& context); 
+void test_i2f_f2i(API& context);
+void test_get_put(API& context);
 
 int main()
 {
     API context;
     try
     {
-        test_imm(context);
+        test_get_put(context);
     }
     catch(const char* str)
     {
         std::cerr << str << '\n';
     }
 }
+
+void test_get_put(API& context)
+{
+    /* // get_put.tc
+    let x = geti();
+    let y = x + 1;
+    let _ = puti(y);
+     */
+    context.BlockBegin();
+
+    context.AppBeg(context.ExprVar("geti"));
+    
+    auto stmt = context.Let("x", context.ExprAppEnd());
+    
+    context.BlockStmt(stmt
+        
+    );
+
+    context.BlockStmt(
+        context.Let("y", context.BinOp(context.ExprVar("x"), tc::ast::Oper::Add, context.I(1)))
+    );
+    context.AppBeg(context.ExprVar("puti"));
+    context.AppArg(context.ExprVar("y"));
+    context.BlockStmt(
+        context.Let("_", context.ExprAppEnd())
+    );
+    
+    context.save(context.BlockEnd());
+    
+    
+    context.save("get_put.hex");
+    
+}
+
 void test_i2f_f2i(API& context)
 {
     /* // i2f_f2i.tc
@@ -160,11 +195,13 @@ void test_imm(API& context)
     context.BlockStmt(context.Let("i", context.I(1)));
     context.BlockStmt(context.Let("f", context.F(0.0)));
     context.BlockStmt(context.Let("h", context.S("Hello!\n")));
+    
     context.BlockStmt(
         context.Let("s", context.S(
             "To be, or not to be, that is the question:\n"
         ))
     );
+    
     auto stmt = context.BlockEnd();
     context.save(stmt);
     context.save("imm.hex");
